7c08e318e6ce92aa7f43c672db72b97746a39a1b
Simon Frezard <simon.frezard@gmail.com>
2020-12-17 13:50:43 +0100

    fused branches

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
     <div  class="w3-center">
         <h1>Music Workout</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur commodi culpa cupiditate dolore doloremque eius eos est, et explicabo fuga harum ipsum perferendis, quaerat recusandae reiciendis sapiente sed tempore totam?</p>
-        
+
     </div>
 </template>
 
